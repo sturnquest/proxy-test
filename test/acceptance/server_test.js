4d9e9eea7136086baea249c3793a7ab6a23e2960
sturnquest <sturnquest@gmail.com>
2014-10-29 22:37:42 -0700

    $1

--- test/acceptance/server_test.js
+++ test/acceptance/server_test.js
@@ -3,12 +3,27 @@ const expect = chai.expect;
 
 const url = require("url");
 const request = require('request');
+const exec = require('child_process').exec;
+const sleep = require('sleep');
 
 var proxyBaseUrl = "http://localhost:2000";
 var replayBaseUrl = "http://localhost:8080/replay";
+var child;
+
+beforeEach(function() {
+    child = exec("ruby ~/projects/proxy/app/transparent_proxy.rb");
+
+    // Don't like haveing to sleep here but we need to allow some time for the process to startup
+    // before we hit the proxy server
+    sleep.sleep(1);
+})
+
+afterEach(function() {
+    child.kill();
+});
 
 
-describe("Proxy", function(){
+describe("Proxy", function() {
 
     it("forwards request headers", function(done) {
 
@@ -20,7 +35,6 @@ describe("Proxy", function(){
 
         // Make a request to the proxy
         request({url: proxyUrl, headers: expectedRequestHeaders}, function(error) {
-            console.log("i'm here!");
             expect(error).to.not.exist;
 
             // Now check that the proxy forwarded the request headers to our destination server
@@ -28,7 +42,6 @@ describe("Proxy", function(){
             request(replayUrl, function(error, response, body) {
                 expect(error).to.not.exist;
 
-                console.log(body);
                 var content = JSON.parse(body);
                 var requestHeaders = content.request.headers;
 
